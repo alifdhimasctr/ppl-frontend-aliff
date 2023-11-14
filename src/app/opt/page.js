70858eb71f3f20988533a59dda01ef90ab1365d0
Alif Dhimas <alifdhimasctr@gmail.com>
2023-11-14 17:30:09 +0700

    update dashboard

--- src/app/opt/page.js
+++ src/app/opt/page.js
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import Image from "next/image";
 import BaseLayout_dpt from "@/components/BaseLayout/BaseLayout_opt";
 import { PiStudentFill } from "react-icons/pi";
 import { PiChalkboardTeacherFill } from "react-icons/pi";
@@ -9,6 +10,19 @@ import CountUp, { Number } from "@/components/Number";
 export default function page() {
   return (
     <BaseLayout_dpt>
+      <h1 className="text-2xl font-bold text-gray-700 mb-3">DASHBOARD</h1>
+      <div className="profil_dashboard-container1">
+        <Image   
+          src="/profil_photo.jpg" 
+          width={80} height={80} className="dashboard_profil-img"
+          alt="profil"
+        />
+        <h1 className="text-2xl font-semibold text-white">REFIOLA JULIETA</h1>
+      </div>
+      <div className="profil_dashboard-container2">
+        <h1 className="text-lg text-black">NIP : 19821462187921389119</h1>
+      </div>
+
       <div className="count_member_container">
         <div className="count_member">
           <div className="count_member-icon">
