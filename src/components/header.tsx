07bbb121bdcd2feaa67317e3d42bbb7dc04504a7
agsry <agungkdp208@gmail.com>
2023-11-16 21:41:24 +0700

    dropdown profile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import Image from 'next/image'
+"use client";
+
+import { Avatar, Dropdown, Navbar } from "flowbite-react";
 
 export default function Header() {
   return (
-    <nav className='bg-white flex justify-between items-center h-12 p-2'>
-        <div className='header_name'>Welcome, Refiola Julieta</div>
-            <div className="header_profil">
-                <Image src="/profil_photo.jpg" width={80} height={80} alt="profile" className="header_profil-img"/>
-                <div className="header_profil-name">Refiola Julieta</div>
-        </div>
-
-    </nav>
-  )
+    <Navbar fluid rounded>
+      <div className="flex justify-end w-full md:order-2">
+        <Dropdown
+          className="flex justify-end"
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar
+              className="mr-11"
+              alt="User settings"
+              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+              rounded
+            />
+          }
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">
+              name@flowbite.com
+            </span>
+          </Dropdown.Header>
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item>Earnings</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
+        <Navbar.Toggle />
+      </div>
+    </Navbar>
+  );
 }
