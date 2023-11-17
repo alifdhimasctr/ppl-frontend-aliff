60ec5e6ca8130e73aa275a6abbdfe9307202792f
agsry <agungkdp208@gmail.com>
2023-11-17 10:35:05 +0700

    vector dan lp

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,26 @@
+import Link from "next/link";
 export default function Home() {
   return (
-    <div className="container w-full h-screen bg-[#183D3D]">
-      <div className="kata w-3/6 text-[#f5f5f5]">
-        <h1 className="font-bold text-[34px]">
-          Selamat Datang di, <br></br>Sistem Akademik PPL BANGKIT
+    <div className="container flex w-full h-screen bg-[#183D3D]">
+      <div className="kata ml-16 mt-72 w-3/6 text-[#f5f5f5]">
+        <h1 className="font-bold text-[36px]">
+          Selamat Datang di, <br></br>
+          <span className="text-[#183D3D] bg-[#f5f5f5] p-1">
+            Sistem Informasi Akademik
+          </span>
+          <span className="text-[#ffd700]"> PPL BANGKIT</span>
         </h1>
         <p className="mt-5">
           Pengelolaan Sistem Informasi Akademik Untuk Mahasiswa, Dosen Wali,
           Operator dan Departemen
         </p>
-        <button className="bg-white px-5 py-2 text-black rounded-xl">
-          Login
-        </button>
+        <Link href="/login">
+          <button className="bg-white px-5 py-2 text-black rounded-xl mt-11">
+            Lanjutkan ke Halaman Login
+          </button>
+        </Link>
       </div>
+      <img className="ml-11" src="vector.png" alt="" />
     </div>
   );
 }
