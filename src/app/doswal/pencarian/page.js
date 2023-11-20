b727ac0570162ae006e0e57cf7b63c16587011c1
agsry <agungkdp208@gmail.com>
2023-11-20 09:05:27 +0700

    lupa

--- src/app/doswal/pencarian/page.js
+++ src/app/doswal/pencarian/page.js
@@ -3,5 +3,16 @@ import BaseLayout_doswal from "@/components/BaseLayout/BaseLayout_doswal";
 import React from "react";
 
 export default function PencarianDoswal() {
-  return <BaseLayout_doswal>Ini adalah halaman pencarian</BaseLayout_doswal>;
+  return (
+    <BaseLayout_doswal>
+      {" "}
+      <h1 className="text-4xl font-semibold text-[#183D3D] mt-5 mb-7">
+        Pencarian
+        <span className="px-2 py-1 bg-[#183d3d] rounded text-[#f5f5f5] ml-2">
+          Mahasiswa
+        </span>
+      </h1>
+      
+    </BaseLayout_doswal>
+  );
 }
