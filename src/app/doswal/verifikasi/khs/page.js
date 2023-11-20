11e805defe12dba8c36c79b4921fa3f6bfb7bfb5
agsry <agungkdp208@gmail.com>
2023-11-20 18:42:28 +0700

    verifikasi

---
+++ src/app/doswal/verifikasi/khs/page.js
@@ -0,0 +1,52 @@
+"use client";
+import BaseLayout_doswal from "@/components/BaseLayout/BaseLayout_doswal";
+import React from "react";
+import { Button } from "flowbite-react";
+import Link from "next/link";
+
+export default function VerifikasiKhsDoswal() {
+  return (
+    <BaseLayout_doswal>
+      {" "}
+      <h1 className="text-4xl font-semibold text-[#183D3D] mt-5 mb-7">
+        Verifikasi
+        <span className="px-2 py-1 bg-[#183d3d] rounded text-[#f5f5f5] ml-2">
+          Mahasiswa
+        </span>
+      </h1>
+      <Button.Group className="mt-7">
+        <Link href="/doswal/verifikasi/irs">
+          <Button color="gray">IRS</Button>
+        </Link>
+        <Link href="/doswal/verifikasi/khs">
+          <Button className="font-bold text-white bg-[#183d3d]" color="gray">
+            KHS
+          </Button>
+        </Link>
+        <Link href="/doswal/verifikasi/pkl">
+          <Button color="gray">PKL</Button>
+        </Link>
+        <Link href="/doswal/verifikasi/skripsi">
+          <Button color="gray">Skripsi</Button>
+        </Link>
+      </Button.Group>
+      {/* Table */}
+      <table className="mt-11 w-full">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Nim</th>
+            <th>Angkatan</th>
+            <th>Semester</th>
+            <th>IPK</th>
+            <th>SKSK</th>
+            <th>File</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>{/* Add table rows here if you have data to display */}</tbody>
+      </table>
+    </BaseLayout_doswal>
+  );
+}
--- src/app/doswal/verifikasi/page.js
+++ src/app/doswal/verifikasi/page.js
@@ -1,7 +1,27 @@
 "use client";
 import BaseLayout_doswal from "@/components/BaseLayout/BaseLayout_doswal";
 import React from "react";
+import { Button } from "flowbite-react";
+import Link from "next/link";
 
 export default function VerifikasiDosenwali() {
-  return <BaseLayout_doswal>Ini adalah halaman verifikasi</BaseLayout_doswal>;
+  return (
+    <BaseLayout_doswal>
+      {" "}
+      <h1 className="text-4xl font-semibold text-[#183D3D] mt-5 mb-7">
+        Verifikasi
+        <span className="px-2 py-1 bg-[#183d3d] rounded text-[#f5f5f5] ml-2">
+          Mahasiswa
+        </span>
+      </h1>
+      <Button.Group>
+        <Link href="/doswal/verifikasi/irs">
+          <Button color="gray">IRS</Button>
+        </Link>
+        <Button color="gray">KHS</Button>
+        <Button color="gray">PKL</Button>
+        <Button color="gray">Skripsi</Button>
+      </Button.Group>
+    </BaseLayout_doswal>
+  );
 }
