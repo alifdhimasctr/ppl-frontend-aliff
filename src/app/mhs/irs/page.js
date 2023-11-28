5c8684a8094783ba807d5bfa3e599ee9763ae38c
raihan ridho <mraihanrdh21@gmail.com>
2023-11-28 23:40:22 +0700

    irs

--- src/app/mhs/irs/page.js
+++ src/app/mhs/irs/page.js
@@ -200,7 +200,7 @@ const Irs = () => {
                           className="block w-min mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                           id="default_size"
                           type="file"
-                          onChange={(event) => handleChange(event, "file")}
+                          onChange={(event) => handleFileChange(event, "file")}
                           
                         ></input>
                       </label>
