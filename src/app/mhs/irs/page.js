b14147dfa0b6ecba1f99a1132ad861243f607ddd
raihan ridho <mraihanrdh21@gmail.com>
2023-11-28 12:11:00 +0700

    irsssss

--- src/app/mhs/irs/page.js
+++ src/app/mhs/irs/page.js
@@ -11,26 +11,29 @@ const Irs = () => {
   const router = useRouter();
   const [cookies, setCookie] = useCookies(["token"]);
   const [formData, setFormData] = useState({
-    semester : "",
-    sks : "",
+    semester: "",
+    sks: "",
+    file: null,
   });
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-
     try {
+
       const res = await axios.post("http://localhost:4000/irs", {
         semester: formData.semester,
         sks: formData.sks,
+        file: formData.file,
       },
-
       {headers:{
+        "Content-Type": "multipart/form-data",
         Authorization: `Bearer ${cookies.token}`,
       }});
+
       console.log("Response:", res);
 
-      if (res.status === 200) {
+      if (res.status === 201) {
         // Show success alert
         alert("IRS entry successful!");
         router.push("/mhs/irs");
@@ -39,17 +42,16 @@ const Irs = () => {
         alert("Failed to save IRS entry. Please try again.");
       }
     } catch (error) {
-      console.log("Error:", error);
+      console.error("Error:", error);
       // Show error alert
       alert("An error occurred. Please try again.");
     }
   };
 
   const handleChange = (event,field) => {
-    const {value} = event.target;
-    setFormData({...formData, [field]: value});
+    const {value,files} = event.target;
+    setFormData({...formData, [field]: value,files});
   };
-
 
   return (
     <BaseLayout>
@@ -155,7 +157,42 @@ const Irs = () => {
 
                   <div className="mb-10"> </div>
                   <div className="flex mb-4 items-center justify-center w-full">
-                    
+                    <label
+                      htmlFor="dropzone-file"
+                      className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                    >
+                      <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg
+                          className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 20 16"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                          />
+                        </svg>
+                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                          <span className="font-semibold">Click to upload</span>{" "}
+                          or drag and drop
+                        </p>
+                        <p className="text-xs text-gray-500 dark:text-gray-400">
+                          PDF,PNG, JPG, or GIF (MAX. 800x400px)
+                        </p>
+                      </div>
+                      <input
+                        className="block w-min mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                        id="default_size"
+                        type="file"
+                        value={file}
+                        onChange={(event) => handleChange(event, "file")}
+                        
+                      ></input>
+                    </label>
                   </div>
                   <div className="mb-1">
                     <button type="submit" className="m-1 ml-0 bg-[#183d3d] rounded-md text-white w-20 h-10 ">
@@ -175,3 +212,4 @@ const Irs = () => {
 };
 
 export default Irs;
+
